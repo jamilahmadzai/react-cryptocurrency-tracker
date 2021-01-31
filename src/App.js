d188487c84239d01adc3046ddfc9553f1db61a4e
Jamil Ur Rehman Ahmadzai <jamilurrehman722@gmail.com>
2021-01-31 06:15:26 +0100

    added input for filtering coins

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Coin from "./Coin";
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
 
   const api =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false";
@@ -14,9 +15,27 @@ function App() {
     });
   }, []);
 
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const filterdCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(search.toLowerCase());
+  });
+
   return (
     <div className="App">
-      {coins.map((coin) => {
+      <form>
+        <input
+          className="search"
+          value={search}
+          type="text"
+          placeholder="Search for Coins"
+          onChange={handleChange}
+        />
+      </form>
+      {filterdCoins.map((coin) => {
         return <Coin key={coin.id} {...coin} />;
       })}
     </div>
